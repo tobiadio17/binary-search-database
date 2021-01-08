f57a74200b7a1f998921e6ec40a5659b0a863c32
Tobi Adio <74212845+tobiadio17@users.noreply.github.com>
2021-01-08 02:01:37 -0600

    Add files via upload

---
+++ bst.java
@@ -0,0 +1,169 @@
+class bst{
+    
+    Node root;
+
+    private class Node{
+    	
+    	// These attributes of the Node class will not be sufficient for those attempting the AVL extra credit.
+    	// You are free to add extra attributes as you see fit, but do not remove attributes given as it will mess with help code.
+        String keyword;
+        Record record;
+        int size;
+        Node l;
+        Node r;
+
+        private Node(String k){
+        	// TODO Instantialize a new Node with keyword k.
+        	keyword = k;
+        }
+
+        private void update(Record r){
+        	//TODO Adds the Record r to the linked list of records. You do not have to check if the record is already in the list.
+        	//HINT: Add the Record r to the front of your linked list.
+        	if (record == null) {
+				record = r;
+			} else {
+				r.next = record;
+				record = r;
+			}
+		}
+
+     }
+
+       
+    
+
+    public bst(){
+        this.root = null;
+    }
+      
+    public void insert(String keyword, FileData fd){
+        Record recordToAdd = new Record(fd.id, fd.author, fd.title, null);
+        //TODO Write a recursive insertion that adds recordToAdd to the list of records for the node associated
+        //with keyword. If there is no node, this code should add the node.
+        keyword = keyword.trim();
+		insert(keyword, recordToAdd, root);
+    }
+    
+    private void insert(String keyword, Record recordToAdd, Node root) {
+		// checks if root is null 
+		if (root == null) {
+			// adds new node to the root
+			Node node = new Node(keyword);
+			node.update(recordToAdd);
+			this.root = node;
+		} else if (keyword.compareTo(root.keyword) < 0) {
+			// checks if at the end of tree
+			if (root.l != null) {
+				insert(keyword, recordToAdd, root.l);
+			} else {
+				Node node = new Node(keyword);
+				node.update(recordToAdd);
+				root.l = node;
+			}
+		} else if (keyword.compareTo(root.keyword) > 0) {
+			if (root.r != null) {
+				insert(keyword, recordToAdd, root.r);
+			} else {
+				Node node = new Node(keyword);
+				node.update(recordToAdd);
+				root.r = node;
+			}
+		} else {
+			root.update(recordToAdd);
+		}
+	}
+    
+    
+    public boolean contains(String keyword){
+    	//TODO Write a recursive function which returns true if a particular keyword exists in the bst
+    	return contains(keyword, root);
+    }
+    
+    private boolean contains(String keyword, Node root){
+		// checks if root is empty
+		if(root == null) 
+			return false;
+		if(keyword.compareTo(root.keyword) < 0){
+			return contains(keyword, root.l);
+		} else if (keyword.compareTo(root.keyword) > 0){
+			 return contains(keyword, root.r);
+		} else {
+			return true;
+		}
+	}
+    
+    
+    public Record get_records(String keyword){
+        //TODO Returns the first record for a particular keyword. This record will link to other records
+    	//If the keyword is not in the bst, it should return null.
+    	Node current = root;
+		while(current != null){
+			if(keyword.compareTo(current.keyword) < 0){
+				current = current.l;
+			} else if (keyword.compareTo(current.keyword) > 0){
+				current = current.r;
+			} else {
+				return current.record;
+			}
+		}
+    	return null;
+    }
+
+    public void delete(String keyword){
+    	//TODO Write a recursive function which removes the Node with keyword from the binary search tree.
+    	//You may not use lazy deletion and if the keyword is not in the bst, the function should do nothing.
+    	delete(keyword, root);
+    }
+    
+    private Node delete(String keyword, Node current){
+		if (current == null){
+		} else if(keyword.compareTo(current.keyword) < 0){
+			current.l = delete(keyword, current.l);
+		} else if(keyword.compareTo(current.keyword) > 0) {
+			current.r = delete(keyword, current.r);
+		} else {
+			if(current.r == null){
+				current = current.l;
+			} else {
+				Node replacement = lowest(current.r);
+				// move values into node 
+				current.keyword = replacement.keyword;
+				current.record = replacement.record;
+				current.size = replacement.size;
+				// delete the smallest node
+				current.r = delete(replacement.keyword, current.r);
+			}
+		}
+		return current;
+	}
+
+    public void print(){
+        print(root);
+    }
+
+    private void print(Node t){
+        if (t!=null){
+            print(t.l);
+            System.out.println(t.keyword);
+            Record r = t.record;
+            while(r != null){
+                System.out.printf("\t%s\n",r.title);
+                r = r.next;
+            }
+            print(t.r);
+        } 
+    }
+    
+    private Node lowest(Node root){
+		if(root == null){
+			return null;
+		}
+		if(root.l == null){
+			return root;
+		}
+		return lowest(root.l);
+	}
+    
+
+}
